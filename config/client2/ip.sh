8280b7ed4665351f07984050a6484e861825ad72
thcorre <correthomas@gmail.com>
2024-02-29 17:08:37 +0800

    Create ip.sh

---
+++ config/client2/ip.sh
@@ -0,0 +1,7 @@
+# Add IPv4
+ip address add 172.17.44.2/30 dev eth1
+ip route add 172.17.11.0/30 via 172.17.44.1
+
+# Add IPv6
+ip -6 address add 2002::172:17:44:2/127 dev eth1
+ip -6 route add 2002::172:17:11:0/127 via 2002::172:17:44:1
