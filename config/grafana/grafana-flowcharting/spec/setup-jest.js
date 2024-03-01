fcdd9719144713841ce4f2f4c72d7e672ff78c5a
thcorre <correthomas@gmail.com>
2024-03-01 09:54:52 +0800

    Add files via upload

---
+++ config/grafana/grafana-flowcharting/spec/setup-jest.js
@@ -0,0 +1,16 @@
+// setup-jest.js
+
+import lodash from 'lodash';
+global._ = lodash;
+
+import $ from 'jquery';
+global.$ = $;
+
+// import * as moment from 'moment';
+// global.moment = moment;
+
+import angular from 'angular';
+global.angular = angular;
+
+// import GFP from '../src/plugin';
+// global.GFP = GFP;
